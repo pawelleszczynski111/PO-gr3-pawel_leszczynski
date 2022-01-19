ba7ed7558cbc640281cbb47b019c062e4cc4d1ae
pawelleszczynski111 <73653376+pawelleszczynski111@users.noreply.github.com>
2022-01-19 18:18:04 +0100

    Add files via upload

---
+++ lab1/src/main.java
@@ -0,0 +1,55 @@
+public class main {
+    public static void main(String[] args) {
+//        zad1
+//        int styczen, luty, marzec, suma;
+//        styczen=31;
+//        luty=29;
+//        marzec=31;
+//        suma=styczen+luty+marzec;
+//        System.out.println("suma pierwszch trzech miesiecy ="+suma);
+//
+//        zad2
+//                int i,suma,liczba;
+//                liczba=1;
+//                i=0;
+//                suma=0;
+//                while(i<11){
+//                    suma=suma+liczba+1;
+//                    i=i+1;
+//                }
+//        System.out.println("suma 10 pierwszych liczb="+suma);
+//
+//
+//        zad3
+//        int i, suma = 1;
+//        for (i = 1; i < 11; i++) {
+//            suma = suma * i;
+//        }
+//        System.out.println("iloczyn=" + suma);
+//
+//        zad4
+//        double rok1=1000,rok2,rok3;
+//        rok2=rok1+(0.06*rok1);
+//        rok3=rok2+(0.06*rok2);
+//        System.out.println("na koncie jest"+rok3);
+//
+//        zad5
+//        System.out.println(" ----");
+//        System.out.println("|java|");
+//        System.out.println(" ----");
+//
+//        zad6
+//        System.out.println("  //////");
+//        System.out.println(" +mmmmmm+");
+//        System.out.println("(| o  o |)");
+//        System.out.println("   | u |");
+//
+//        zad7
+        System.out.println("***    *       *      *   ***");
+        System.out.println("* *   *  *     *      *   *"  );
+        System.out.println("***   ****     *      *   ***");
+        System.out.println("*     *  *     *  *   *   *  ");
+        System.out.println("*     *  *     * *  * *   *"  );
+        System.out.println("*     *  *     *      *   ***"    );
+    }
+}
